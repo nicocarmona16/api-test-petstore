85371d05772ddc20c773aa1bfd75e0c5f778217e
nicocarmona16 <nicolas.carmona@globant.com>
2026-03-17 23:11:36 -0500

    test: implement create order API test

---
+++ src/test/java/com/perfdog/automation/model/OrderDTO.java
@@ -0,0 +1,21 @@
+package com.perfdog.automation.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OrderDTO {
+    private Long id;
+    private Long petId;
+    private Integer quantity;
+    private String shipDate;
+    private String status;
+    private Boolean complete;
+}
---
+++ src/test/java/com/perfdog/automation/test/CreateOrderTest.java
@@ -0,0 +1,50 @@
+package com.perfdog.automation.test;
+
+import com.perfdog.automation.config.TestRunner;
+import com.perfdog.automation.model.CategoryDTO;
+import com.perfdog.automation.model.OrderDTO;
+import com.perfdog.automation.model.PetDTO;
+import com.perfdog.automation.model.TagDTO;
+import com.perfdog.automation.request.RequestBuilder;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+
+public class CreateOrderTest extends TestRunner {
+
+    private PetDTO orderPet;
+
+    @BeforeMethod
+    public void setupData() {
+        orderPet = PetDTO.builder()
+                .id(123456789123L)
+                .category(CategoryDTO.builder().id(1L).name("Dogs").build())
+                .name("Test Dog")
+                .status("available")
+                .build();
+        RequestBuilder.postRequest(getBaseurl(), "/pet", orderPet);
+    }
+
+    @Test(testName = "Create order to buy a pet")
+    public void testCreateOrder() {
+        OrderDTO newOrder = OrderDTO.builder()
+                .id(102030L)
+                .petId(orderPet.getId())
+                .quantity(1)
+                .shipDate("2026-03-18T00:00:00.000Z")
+                .status("placed")
+                .complete(true)
+                .build();
+
+        Response response = RequestBuilder.postRequest(getBaseurl(), "/store/order", newOrder);
+        OrderDTO actualOrder = response.as(OrderDTO.class);
+
+        Assert.assertEquals(response.statusCode(), 200, "El status code no es 200 OK");
+        Assert.assertEquals(actualOrder.getId(), newOrder.getId(), "El ID de la orden no coincide");
+        Assert.assertEquals(actualOrder.getPetId(), newOrder.getPetId(), "El ID de la mascota no coincide");
+        Assert.assertEquals(actualOrder.getStatus(), "placed", "El estado de la orden no es placed");
+    }
+}
